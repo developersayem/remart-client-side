815b71aad09ff74b04e8c267e6d2ebdd2c86418f
devsayemmolla <98878071+devsayemmolla@users.noreply.github.com>
2022-11-24 15:01:43 +0600

    first commit

---
+++ src/Routes/Route/Route.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { createBrowserRouter } from "react-router-dom";
+import Main from '../../Layouts/Main/Main';
+import Register from '../../Pages/Authentication/Register';
+
+
+export const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Main></Main>,
+        children: [
+            {
+                path: '/register',
+                element: <Register></Register>
+            }
+        ]
+    }
+])
+
+
+const Route = () => {
+    return (
+        <div>
+        </div>
+    );
+};
+
+export default Route;
---
+++ src/Shared/NavBar.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Navbar, Button } from 'flowbite-react';
+import { Link } from 'react-router-dom';
+
+
+const NavBar = () => {
+    return (
+        <Navbar className="mx-10"
+            fluid={true}
+            rounded={true}
+        >
+            <Link to='/'>
+                <h1 className="bg-gradient-to-r italic font-mono font-bold from-blue-600 via-red-600 to-blue-500 inline-block text-transparent bg-clip-text text-3xl">ReMart</h1>
+            </Link>
+            <div className="flex md:order-2">
+                <Button className='hidden' gradientMonochrome="info">
+                    Avater
+                </Button>
+                <Button className='hidden md:block mx-2' gradientMonochrome="info">
+                    <Link
+                        to='/logout'>
+                        Log Out
+                    </Link>
+                </Button>
+                <Button className='hidden md:block mx-2' gradientMonochrome="info">
+                    <Link
+                        to='/register'>
+                        Register
+                    </Link>
+                </Button>
+                <Navbar.Toggle />
+            </div>
+            <Navbar.Collapse>
+                <Link
+                    to='/home'>
+                    Home
+                </Link>
+                <Link
+                    to='/blogs'>
+                    Blogs
+                </Link>
+            </Navbar.Collapse>
+        </Navbar>
+    );
+};
+
+export default NavBar;
